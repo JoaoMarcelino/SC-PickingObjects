2f2f7633d2d33857a773c29017100d52d95a2c4c
JoaoMarcelino <j.13.marcelino@gmail.com>
2022-05-05 20:38:26 +0100

    mais testes e plots

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def main():
     n_ants = [1] + [a for a in range(10, 60, 10)] 
     n_sticks = [a for a in range(50, 300, 50)] 
     neigh = ["VN","M", "IVN"]
-    stick_min = [1, 2, 5]
-    stick_max = [5, 10, 15, 0]
+    stick_min = [5]
+    stick_max = [10, 15, 0]
     width = 10
     height = 10
     num_gens = 1000
@@ -32,9 +32,6 @@ def main():
                 for s_min in stick_min:
                     for s_max in stick_max:
 
-
-                        if s_max == s_min:
-                            break
                         
                         total = f"batch_{ants}_{sticks}_{neighbor}_{s_min}_{s_max}.csv"
                         path_to_file = path + total
@@ -71,7 +68,7 @@ def main():
 def readfile(file):
     return pd.read_csv(file)
 
-def plotfiles():
+def plotAnts(sticks):
 
     n_ants = [1] + [a for a in range(10, 60, 10)] 
     n_sticks = [a for a in range(50, 300, 50)] 
@@ -88,7 +85,6 @@ def plotfiles():
     min = stick_min[0]
     max = stick_max[0]
     n = neigh[0]
-    sticks = n_sticks[2]
 
 
     size_ants = len(n_ants)
@@ -121,6 +117,222 @@ def plotfiles():
         average = average/seed
 
         axs.plot(steps, average,  label=f'{ants}')
+        axs.legend(loc="upper right")
+
+
+    plt.show()
+
+def plotSticks(ants):
+
+    n_ants = [1] + [a for a in range(10, 60, 10)] 
+    n_sticks = [a for a in range(50, 300, 50)] 
+    neigh = ["VN","M", "IVN"]
+    stick_min = [1, 2, 5]
+    stick_max = [0, 5, 10, 15]
+    width = 10
+    height = 10
+    num_gens = 1000
+    seed = 30
+
+    path = "./SC-PickingObjects/results/"
+
+    min = stick_min[0]
+    max = stick_max[0]
+    n = neigh[0]
+
+
+    size_ants = len(n_ants)
+    size_sticks = len(n_sticks)
+    fig, axs = plt.subplots(1, 1, sharey=True, sharex=True)
+    fig.suptitle(f"Alteração de sticks para {ants} ants")
+
+    plt.xlabel("Number of Generations")
+    plt.ylabel("Average of Sticks")
+
+    for i, sticks in enumerate(n_sticks):
+        total = f"batch_{ants}_{sticks}_{n}_{min}_{max}.csv"
+        df = readfile(path + total)
+
+        step0 = (df.Step== 0) & (df.iteration == 0)
+        agentId = (df.AgentID == 0) & (df.iteration == 0)
+
+        steps =  df[(step0) | (agentId)].Step.values
+        average = df[(step0) | (agentId)].Average.values
+        
+        for j in range(1, seed):
+
+            step0 = (df.Step== 0) & (df.iteration == j)
+            agentId = (df.AgentID == 0) & (df.iteration == j)
+
+            df_average =  df[step0 | agentId].Average.values
+
+            average = np.sum([average, df_average], axis=0)
+
+        average = average/seed
+
+        axs.plot(steps, average,  label=f'{sticks}')
+        axs.legend(loc="upper right")
+
+
+    plt.show()
+
+
+
+def plotNeigh(ants, sticks):
+
+    n_ants = [1] + [a for a in range(10, 60, 10)] 
+    n_sticks = [a for a in range(50, 300, 50)] 
+    neigh = ["VN","M", "IVN"]
+    stick_min = [1, 2, 5]
+    stick_max = [0, 5, 10, 15]
+    width = 10
+    height = 10
+    num_gens = 1000
+    seed = 30
+
+    path = "./SC-PickingObjects/results/"
+
+    min = stick_min[0]
+    max = stick_max[0]
+
+
+
+    size_ants = len(n_ants)
+    size_sticks = len(n_sticks)
+    fig, axs = plt.subplots(1, 1, sharey=True, sharex=True)
+    fig.suptitle(f"Alteração de Neighborhood para {ants} ants e {sticks} sticks")
+
+    plt.xlabel("Number of Generations")
+    plt.ylabel("Average of Sticks")
+
+    for i, n in enumerate(neigh):
+        total = f"batch_{ants}_{sticks}_{n}_{min}_{max}.csv"
+        df = readfile(path + total)
+
+        step0 = (df.Step== 0) & (df.iteration == 0)
+        agentId = (df.AgentID == 0) & (df.iteration == 0)
+
+        steps =  df[(step0) | (agentId)].Step.values
+        average = df[(step0) | (agentId)].Average.values
+        
+        for j in range(1, seed):
+
+            step0 = (df.Step== 0) & (df.iteration == j)
+            agentId = (df.AgentID == 0) & (df.iteration == j)
+
+            df_average =  df[step0 | agentId].Average.values
+
+            average = np.sum([average, df_average], axis=0)
+
+        average = average/seed
+
+        axs.plot(steps, average,  label=f'{n}')
+        axs.legend(loc="upper right")
+
+
+    plt.show()
+
+def plotStickMin(ants, sticks):
+
+    n_ants = [1] + [a for a in range(10, 60, 10)] 
+    n_sticks = [a for a in range(50, 300, 50)] 
+    neigh = ["VN","M", "IVN"]
+    stick_min = [1, 2, 5]
+    stick_max = [0, 5, 10, 15]
+    width = 10
+    height = 10
+    num_gens = 1000
+    seed = 30
+
+    path = "./SC-PickingObjects/results/"
+
+    n = neigh[0]
+    max = stick_max[0]
+
+
+
+    size_ants = len(n_ants)
+    size_sticks = len(n_sticks)
+    fig, axs = plt.subplots(1, 1, sharey=True, sharex=True)
+    fig.suptitle(f"Alteração de StickMin para {ants} ants e {sticks} sticks")
+
+    plt.xlabel("Number of Generations")
+    plt.ylabel("Average of Sticks")
+
+    for i, min in enumerate(stick_min):
+        total = f"batch_{ants}_{sticks}_{n}_{min}_{max}.csv"
+        df = readfile(path + total)
+
+        step0 = (df.Step== 0) & (df.iteration == 0)
+        agentId = (df.AgentID == 0) & (df.iteration == 0)
+
+        steps =  df[(step0) | (agentId)].Step.values
+        average = df[(step0) | (agentId)].Average.values
+        
+        for j in range(1, seed):
+
+            step0 = (df.Step== 0) & (df.iteration == j)
+            agentId = (df.AgentID == 0) & (df.iteration == j)
+
+            df_average =  df[step0 | agentId].Average.values
+
+            average = np.sum([average, df_average], axis=0)
+
+        average = average/seed
+
+        axs.plot(steps, average,  label=f'{min}')
+        axs.legend(loc="upper right")
+
+
+    plt.show()
+
+def plotStickMax(ants, sticks):
+
+    n_ants = [1] + [a for a in range(10, 60, 10)] 
+    n_sticks = [a for a in range(50, 300, 50)] 
+    neigh = ["VN","M", "IVN"]
+    stick_min = [1, 2, 5]
+    stick_max = [0, 5, 10, 15]
+    width = 10
+    height = 10
+    num_gens = 1000
+    seed = 30
+
+    path = "./SC-PickingObjects/results/"
+
+    n = neigh[0]
+    min = stick_min[0]
+
+    size_ants = len(n_ants)
+    size_sticks = len(n_sticks)
+    fig, axs = plt.subplots(1, 1, sharey=True, sharex=True)
+    fig.suptitle(f"Alteração de StickMax para {ants} ants e {sticks} sticks")
+
+    plt.xlabel("Number of Generations")
+    plt.ylabel("Average of Sticks")
+
+    for i, max in enumerate(stick_max):
+        total = f"batch_{ants}_{sticks}_{n}_{min}_{max}.csv"
+        df = readfile(path + total)
+
+        step0 = (df.Step== 0) & (df.iteration == 0)
+        agentId = (df.AgentID == 0) & (df.iteration == 0)
+
+        steps =  df[(step0) | (agentId)].Step.values
+        average = df[(step0) | (agentId)].Average.values
+        
+        for j in range(1, seed):
+
+            step0 = (df.Step== 0) & (df.iteration == j)
+            agentId = (df.AgentID == 0) & (df.iteration == j)
+
+            df_average =  df[step0 | agentId].Average.values
+
+            average = np.sum([average, df_average], axis=0)
+
+        average = average/seed
+
+        axs.plot(steps, average,  label=f'{max}')
         axs.legend(loc="upper right")
 
 
@@ -267,14 +479,28 @@ def heatmap_Median():
     fig.tight_layout()
     plt.show()
 
-    
-
-
-
 
 if __name__ == "__main__":
-    #main()
+    main()
 
-    #plotfiles()
-    heatmap_Average()
-    heatmap_Median()
+    n_sticks = [a for a in range(50, 300, 50)] 
+    for sticks in n_sticks:
+        #plotAnts(sticks)
+        pass
+    
+    n_ants = [1] + [a for a in range(10, 60, 10)] 
+    for ants in n_ants:
+        #plotSticks(ants)
+        pass
+
+
+    ant = n_ants[1]
+    sticks = n_sticks[-1]
+
+    plotNeigh(ant, sticks)
+    plotStickMin(ant, sticks)
+    plotStickMax(ant, sticks)
+    
+    
+    #heatmap_Average()
+    #heatmap_Median()
